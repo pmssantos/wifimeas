6c7e292a3b0c5f4e1e0be9b80625b3fc9d79e06f
pmssantos <pedromsds@gmail.com>
2019-11-25 17:17:15 +0000

    modified:   dev/iperf_edge_cloud.py

--- dev/iperf_edge_cloud.py
+++ dev/iperf_edge_cloud.py
@@ -2,13 +2,27 @@
  
 import socket
 from subprocess import Popen, PIPE
-import threading
 import time
+import threading
 
-#HOST='192.168.60.174'
-#PORT=65432
+def iperf_faraway():
+	f2= open("iperf_local.log","w+")
+	process2 = Popen(["iperf3", "-c", "192.168.60.174", "-i", "1", "-p", "5201"], stdout=PIPE)
+	for row in iter(process2.stdout.readline, b''):
+		f2.write(row.rstrip())
+		print(row.rstrip())
+	f2.close() 
 
-process1 = Popen(["iperf", "-c", "iperf.eenet.ee"], stdout=PIPE)
+if __name__ == "__main__":
 
+	x = threading.Thread(target=iperf_faraway)
+	x.start()
 
-process2 = Popen(["iperf", "-c", "192.168.60.174"], stdout=PIPE)
+	f1= open("iperf_ee.log","w+")
+	process1 = Popen(["iperf3", "-c", "bouygues.iperf.fr", "-i", "1", "-p", "5203"], stdout=PIPE)
+	for row in iter(process1.stdout.readline, b''):
+		print(row.rstrip())
+		f1.write(row.rstrip())
+	f1.close() 
+	
+
