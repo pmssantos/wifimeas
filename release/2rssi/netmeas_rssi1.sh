33468a9edb9d652d0c96b6459c16c720c93cca58
it2s-admin <it2s-admin@it2s-pasmo-02.it2s>
2020-12-28 16:40:30 +0000

    All ready

--- release/2rssi/netmeas_rssi1.sh
+++ release/2rssi/netmeas_rssi1.sh
@@ -9,10 +9,19 @@ NC='\033[0m'
 echo $#
 
 if [ $# -le 5 ]; then
-	printf "${RED} Usage: netmeas s|c w|p <#wireless dev in 'iw dev'> <mon number> <channel (eg: 2412)> ${NC}\n"
+	printf "${RED} Usage: netmeas s|c w|p <#wireless dev in 'iw dev' (not needed for 11p)> <mon number (not needed for 11p)> <channel (eg: 2412; not needed for 11p)> ${NC}\n"
 	exit 0
 fi
 echo "All set"
+
+if [ $2 == p ]; then
+	../configure_11p/configure.sh
+elif [[ $1 == "c" ]]; then
+		../configure/make_monitor.sh $3 $4 $5
+	elif
+		../configure/ad_hoc.sh $3 $4 $5
+	fi
+fi
 
 if [[ $1 == "c" ]]; then
 	logfile="../../logs/"$(date +"%Y%m%d_%H%M%S")"_rssi.pcap"
@@ -21,4 +30,3 @@ if [[ $1 == "c" ]]; then
 	
 	sudo tcpdump -i mon1 -w $logfile.pcap &
 fi
-
